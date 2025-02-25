7ab6358c66e40ac146a902dc43e35b6a26edb29b
HyeonSeong <gus8131@naver.com>
2025-02-25 17:16:52 +0900

    Merge 오류 해결

---
+++ src/main/java/project/studyproject/domain/oauth2/util/SocialClientRegistration.java
@@ -0,0 +1,45 @@
+package project.studyproject.domain.oauth2.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SocialClientRegistration {
+    public ClientRegistration naverClientRegistration() {
+
+        return ClientRegistration.withRegistrationId("naver")
+                .clientId("")
+                .clientSecret("")
+                .redirectUri("http://localhost:8080/login/oauth2/code/naver")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .scope("name", "email")
+                .authorizationUri("https://nid.naver.com/oauth2.0/authorize") // 토큰 uri랑 user, auth 는 이렇게 생성할떄 필수임
+                .tokenUri("https://nid.naver.com/oauth2.0/token")
+                .userInfoUri("https://openapi.naver.com/v1/nid/me")
+                .userNameAttributeName("response")
+                .build();
+    }
+
+    public ClientRegistration googleClientRegistration() {
+
+        return ClientRegistration.withRegistrationId("google")
+                .clientId("")
+                .clientSecret("")
+                .redirectUri("http://localhost:8080/login/oauth2/code/google")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .scope("profile", "email")
+                .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
+                .tokenUri("https://www.googleapis.com/oauth2/v4/token")
+                .jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
+                .issuerUri("https://accounts.google.com")
+                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+                .userNameAttributeName(IdTokenClaimNames.SUB)
+                .build();
+    }
+
+
+}
