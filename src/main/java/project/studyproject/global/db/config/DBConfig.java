753fc5afa84137f72247d17e6bb64d6397a4a6fd
HyeonSeong <gus8131@naver.com>
2025-02-27 15:35:30 +0900

    DBConfig 수정

--- src/main/java/project/studyproject/global/db/config/DBConfig.java
+++ src/main/java/project/studyproject/global/db/config/DBConfig.java
@@ -1,24 +1,24 @@
-package project.studyproject.global.db.config;
-
-import org.hibernate.annotations.Bag;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-
-// DB를 yml 파일이 아닌 클래스 파일 형태로 저장할 때
-@Configuration
-public class DBConfig {
-
-    @Bean
-    public DataSource getDataSource(){
-        return DataSourceBuilder.create()
-                .driverClassName("")
-                .url("")
-                .username("")
-                .password("")
-                .build();
-    }
-
-}
+//package project.studyproject.global.db.config;
+//
+//import org.hibernate.annotations.Bag;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
+//
+//// DB를 yml 파일이 아닌 클래스 파일 형태로 저장할 때
+//@Configuration
+//public class DBConfig {
+//
+//    @Bean
+//    public DataSource getDataSource(){
+//        return DataSourceBuilder.create()
+//                .driverClassName("")
+//                .url("")
+//                .username("")
+//                .password("")
+//                .build();
+//    }
+//
+//}
--- src/main/java/project/studyproject/global/db/config/MongoDataBaseConfig.java
+++ src/main/java/project/studyproject/global/db/config/MongoDataBaseConfig.java
@@ -1,60 +1,60 @@
-package project.studyproject.global.db.config;
-
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-
-import javax.sql.DataSource;
-import java.util.HashMap;
-
-@Configuration
-@EnableJpaRepositories(
-        basePackages = "",
-        entityManagerFactoryRef = "mongoEntityManager",
-        transactionManagerRef = "mongoTransaction"
-)
-public class MongoDataBaseConfig {
-    @Bean
-    public PlatformTransactionManager mongoTransaction() {
-
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(mongoEntityManger().getObject());
-
-        return transactionManager;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean mongoEntityManger() {
-
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-
-        em.setDataSource(mongoDataSource());
-        em.setPackagesToScan(new String[]{""}); // 경로 Entity 주소
-        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-
-        HashMap<String, Object> properties = new HashMap<>();
-        properties.put("hibernate.hbm2ddl.auto", "update");
-
-        em.setJpaPropertyMap(properties);
-
-        return em;
-    }
-
-
-    @Bean
-    public DataSource mongoDataSource(){
-        return DataSourceBuilder.create()
-                .driverClassName("com.mysql.jdbc.Driver")
-                .url("jdbc:mysql://localhost:3306/studyproject")
-                .username("root")
-                .password("root")
-                .build();
-    }
-}
-
+//package project.studyproject.global.db.config;
+//
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.orm.jpa.JpaTransactionManager;
+//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+//import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+//import org.springframework.transaction.PlatformTransactionManager;
+//
+//import javax.sql.DataSource;
+//import java.util.HashMap;
+//
+//@Configuration
+//@EnableJpaRepositories(
+//        basePackages = "",
+//        entityManagerFactoryRef = "mongoEntityManager",
+//        transactionManagerRef = "mongoTransaction"
+//)
+//public class MongoDataBaseConfig {
+//    @Bean
+//    public PlatformTransactionManager mongoTransaction() {
+//
+//        JpaTransactionManager transactionManager = new JpaTransactionManager();
+//        transactionManager.setEntityManagerFactory(mongoEntityManger().getObject());
+//
+//        return transactionManager;
+//    }
+//
+//    @Bean
+//    public LocalContainerEntityManagerFactoryBean mongoEntityManger() {
+//
+//        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+//
+//        em.setDataSource(mongoDataSource());
+//        em.setPackagesToScan(new String[]{""}); // 경로 Entity 주소
+//        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+//
+//        HashMap<String, Object> properties = new HashMap<>();
+//        properties.put("hibernate.hbm2ddl.auto", "update");
+//
+//        em.setJpaPropertyMap(properties);
+//
+//        return em;
+//    }
+//
+//
+//    @Bean
+//    public DataSource mongoDataSource(){
+//        return DataSourceBuilder.create()
+//                .driverClassName("com.mysql.jdbc.Driver")
+//                .url("jdbc:mysql://localhost:3306/studyproject")
+//                .username("root")
+//                .password("root")
+//                .build();
+//    }
+//}
+//
