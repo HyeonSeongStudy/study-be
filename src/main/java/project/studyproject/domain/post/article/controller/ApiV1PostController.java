a9a43053e7c62184393c24d99da482adc53294d6
HyeonSeong <gus8131@naver.com>
2025-03-10 15:28:58 +0900

    feat : ELK 로그 수집 생성

--- src/main/java/project/studyproject/domain/post/article/controller/ApiV1PostController.java
+++ src/main/java/project/studyproject/domain/post/article/controller/ApiV1PostController.java
@@ -1,18 +1,19 @@
 package project.studyproject.domain.post.article.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import project.studyproject.domain.post.article.dto.PostRequest;
 import project.studyproject.domain.post.article.entity.Post;
 import project.studyproject.domain.post.article.service.PostService;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v2/post")
+@Slf4j
 public class ApiV1PostController {
     private final PostService postService;
 
@@ -20,5 +21,34 @@ public class ApiV1PostController {
     public ResponseEntity<Post> write(@RequestBody PostRequest postRequest) {
         return ResponseEntity.ok(postService.write(postRequest.title(),postRequest.content()));
 
+    }
+
+    @GetMapping
+    public String home() {
+        LocalDateTime now = LocalDateTime.now();
+        log.info("[로그 수집 테스트] : {}", now);
+        return "Home" + now;
+    }
+
+    @GetMapping("/logs")
+    public String Logs() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        log.info("Logs " + localDateTime);
+        return "Logs page";
+    }
+
+    @GetMapping("/warn")
+    public String Warn() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        log.warn("Warn" + localDateTime);
+        return "Warn page";
+    }
+
+
+    @GetMapping("/err")
+    public String Error() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        log.error("Error " + localDateTime);
+        return "Error page";
     }
 }
--- src/main/java/project/studyproject/domain/post/entity/PostDoc.java
+++ src/main/java/project/studyproject/domain/post/entity/PostDoc.java
@@ -5,8 +5,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Mapping;
+import org.springframework.data.elasticsearch.annotations.Setting;
 
-@Document(indexName="app1_posts")
+@Document(indexName = "app1_posts", createIndex = true)
+@Setting(settingPath = "/elasticsearch/settings.json")
+@Mapping(mappingPath = "/elasticsearch/mappings.json")
 @Getter
 @Setter
 @Builder
